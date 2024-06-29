14437ffb2df38ede188cfa17dbae6cadc812755a
Katharina Trinley <152901977+KatharinaTrinley@users.noreply.github.com>
2024-06-29 12:43:45 +0200

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -1,36 +1,34 @@
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, LSTM, Conv1D, Dropout, Dense, Flatten, Concatenate
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ReduceLROnPlateau, Callback
-import matplotlib.pyplot as plt
- 
-model = Sequential()
-model.add(LSTM(units=50, return_sequences=True, input_shape=(sequence_length, X_reshaped.shape[2])))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
-model.add(Dense(units=1))
 
+# TCN
+input_layer = Input(shape=(sequence_length, X_train.shape[2]))
+conv1 = Conv1D(filters=64, kernel_size=3, activation='relu')(input_layer)
+dropout1 = Dropout(0.3)(conv1)
+conv2 = Conv1D(filters=64, kernel_size=3, activation='relu')(dropout1)
+dropout2 = Dropout(0.3)(conv2)
+flatten = Flatten()(dropout2)
+
+# LSTM
+lstm1 = LSTM(units=25, return_sequences=True)(input_layer)
+dropout_lstm1 = Dropout(0.2)(lstm1)
+lstm2 = LSTM(units=25)(dropout_lstm1)
+dropout_lstm2 = Dropout(0.2)(lstm2)
+
+merged = Concatenate()([flatten, dropout_lstm2])
+dense1 = Dense(50, activation='relu')(merged)
+output_layer = Dense(1)(dense1)
+model = Model(inputs=input_layer, outputs=output_layer)
 optimizer = Adam(learning_rate=0.001)
 model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mae', 'mse'])
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=1e-7)
+print(model.summary())
 
-history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2, callbacks=[reduce_lr])
+history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2)
 
 y_pred = model.predict(X_test)
-# Inverse transform
-y_pred_inv = scaler_y.inverse_transform(y_pred)
-y_test_inv = scaler_y.inverse_transform(y_test.reshape(-1, 1))
-mae = mean_absolute_error(y_test_inv, y_pred_inv)
-mse = mean_squared_error(y_test_inv, y_pred_inv)
-
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
 print(f'Mean Absolute Error: {mae}')
 print(f'Mean Squared Error: {mse}')
 plt.plot(history.history['loss'], label='Training loss')
