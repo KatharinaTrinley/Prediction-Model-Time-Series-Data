c0c3aa38dedee3be07f9a251e3bd716b24dd89d0
Katharina Trinley <152901977+KatharinaTrinley@users.noreply.github.com>
2024-07-06 15:33:12 +0200

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -8,7 +8,6 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, LSTM, Dropout, Dense
 from tensorflow.keras.optimizers import Adam
 
-# Load and preprocess the data
 data = pd.read_csv('/content/preprocessed.txt')
 print("Original Dataset:")
 print(data.head())
@@ -39,33 +38,26 @@ print(y_reshaped[:2])  # Print first 2 target values corresponding to the sequen
 
 X_train, X_test, y_train, y_test = train_test_split(X_reshaped, y_reshaped, test_size=0.2, random_state=42)
 
-
-# LSTM layers
+# LSTM 
+input_layer = Input(shape=(sequence_length, X_train.shape[2]))
 lstm1 = LSTM(units=50, return_sequences=True)(input_layer)
 dropout_lstm1 = Dropout(0.2)(lstm1)
 lstm2 = LSTM(units=50)(dropout_lstm1)
 dropout_lstm2 = Dropout(0.2)(lstm2)
-
-# Fully connected layers
 dense1 = Dense(50, activation='relu')(dropout_lstm2)
 output_layer = Dense(1)(dense1)
 
-# Create and compile the model
 model = Model(inputs=input_layer, outputs=output_layer)
 optimizer = Adam(learning_rate=0.001)
 model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mae', 'mse'])
 
-# Train the model
 history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2)
-
-# Predict and evaluate the model
 y_pred = model.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 print(f'Mean Absolute Error: {mae}')
 print(f'Mean Squared Error: {mse}')
 
-# Plot training history
 plt.plot(history.history['loss'], label='Training loss')
 plt.plot(history.history['val_loss'], label='Validation loss')
 plt.legend()
