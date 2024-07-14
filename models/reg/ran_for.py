b1094f8d3a4b4b4cf075052e558417052329517f
Katharina Trinley <katharinatrinley@icloud.com>
2024-07-14 16:48:41 +0200

    added data and models

---
+++ models/reg/ran_for.py
@@ -0,0 +1,86 @@
+# install tensorflow==2.13.1 first
+
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+
+from sklearn.metrics import r2_score
+
+# data preparation
+eingang_ts = ['date_EINGANGSDATUM_UHRZEIT', 'time_EINGANGSDATUM_UHRZEIT', 'weekday_EINGANGSDATUM_UHRZEIT']
+verpackt_ts = ['date_VERPACKT_DATUM_UHRZEIT', 'time_VERPACKT_DATUM_UHRZEIT',
+               'weekday_VERPACKT_DATUM_UHRZEIT', 'secs_VERPACKT_DATUM_UHRZEIT']
+auftragsnummer_ts = ['date_AUFTRAGANNAHME_DATUM_UHRZEIT', 'time_AUFTRAGANNAHME_DATUM_UHRZEIT',
+                     'weekday_AUFTRAGANNAHME_DATUM_UHRZEIT', 'secs_AUFTRAGANNAHME_DATUM_UHRZEIT']
+lieferschein_ts = ['date_LIEFERSCHEIN_DATUM_UHRZEIT', 'time_LIEFERSCHEIN_DATUM_UHRZEIT',
+                   'weekday_LIEFERSCHEIN_DATUM_UHRZEIT', 'secs_LIEFERSCHEIN_DATUM_UHRZEIT']
+auftragannahme_ts = ['date_AUFTRAGANNAHME_DATUM_UHRZEIT', 'time_AUFTRAGANNAHME_DATUM_UHRZEIT',
+                     'weekday_AUFTRAGANNAHME_DATUM_UHRZEIT', 'secs_AUFTRAGANNAHME_DATUM_UHRZEIT']
+bereitgestellt_ts = ['date_BEREITGESTELLT_DATUM_UHRZEIT', 'time_BEREITGESTELLT_DATUM_UHRZEIT',
+                     'weekday_BEREITGESTELLT_DATUM_UHRZEIT', 'secs_BEREITGESTELLT_DATUM_UHRZEIT']
+TA_ts = ['weekday_TA_DATUM_UHRZEIT', 'date_TA_DATUM_UHRZEIT', 'time_TA_DATUM_UHRZEIT', 'secs_TA_DATUM_UHRZEIT']
+
+package_data = ['LAENGE_IN_CM', 'BREITE_IN_CM', 'HOEHE_IN_CM', 'GEWICHT_IN_KG', 'count_PACKSTUECKART=BEH',
+                'count_PACKSTUECKART=CAR', 'count_PACKSTUECKART=GBP', 'count_PACKSTUECKART=PAL',
+                'count_PACKSTUECKART=PKI', 'count_PACKSTUECKART=UNKNOWN', 'PACKAGE_COUNT']
+auftragsnummer = ['category_AUFTRAGSNUMMER=DSGA', 'category_AUFTRAGSNUMMER=RBMANUSHIP', 'category_AUFTRAGSNUMMER=return']
+land = ['LAND=AT', 'LAND=AUT', 'LAND=BE', 'LAND=BR', 'LAND=CH', 'LAND=CN', 'LAND=CZ', 'LAND=DE', 'LAND=DK', 'LAND=DR',
+        'LAND=ES', 'LAND=FCA', 'LAND=FR', 'LAND=HU', 'LAND=IE', 'LAND=IN', 'LAND=IT', 'LAND=JP', 'LAND=KR', 'LAND=MX',
+        'LAND=NL', 'LAND=None', 'LAND=PL', 'LAND=RO', 'LAND=RU', 'LAND=TR', 'LAND=UK', 'LAND=US']
+sonderfahrt = ['SONDERFAHRT']
+dienstleister = ['DIENSTLEISTER=DHL', 'DIENSTLEISTER=None', 'DIENSTLEISTER=TNT', 'DIENSTLEISTER=UPS']
+
+step_1_features = eingang_ts + sonderfahrt
+step_2_features = step_1_features + verpackt_ts + auftragsnummer_ts + package_data + auftragsnummer
+step_3_features = step_2_features + land + auftragannahme_ts + auftragannahme_ts + lieferschein_ts
+step_4_features = step_3_features + bereitgestellt_ts
+step_5_features = step_4_features + TA_ts + dienstleister
+
+data = pd.read_csv('preprocessed.txt')
+
+X = data[step_5_features]
+y = np.array(data['PROCESSING'])
+
+scaler_X = StandardScaler()
+X_scaled = scaler_X.fit_transform(X)
+scaler_y = StandardScaler()
+y_scaled = scaler_y.fit_transform(y.reshape(-1, 1))
+
+X_train, X_temp, y_train, y_temp = train_test_split(X_scaled, y_scaled, test_size=0.3, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+def augment_data(X_train, y_train, num_augmentations=3):
+    augmented_X = X_train.copy()
+    augmented_y = y_train.copy()
+
+    for _ in range(num_augmentations):
+        temp_X = X_train.copy()
+        temp_y = y_train.copy()
+
+        # applying random noise to the 'PROCESSING' feature, assumed to be the first feature
+        temp_X[:, 0] += np.random.normal(0, 0.01, size=temp_X[:, 0].shape)
+
+        augmented_X = np.concatenate((augmented_X, temp_X), axis=0)
+        augmented_y = np.concatenate((augmented_y, temp_y), axis=0)
+
+    return augmented_X, augmented_y
+
+X, y = augment_data(X_train, y_train, num_augmentations=3)
+
+from sklearn.ensemble import RandomForestRegressor
+
+forest_model = RandomForestRegressor(n_estimators=5, random_state=42)
+forest_model.fit(X_train, y_train)
+
+y_pred_forest = forest_model.predict(X_test)
+
+mse_forest = mean_squared_error(y_test, y_pred_forest)
+r2_forest = r2_score(y_test, y_pred_forest)
+mae = mean_absolute_error(y_test, y_pred_forest)
+
+print(f"Random Forest Regression - Mean Squared Error: {mse_forest}")
+print(f"Random Forest Regression - R^2 Score: {r2_forest}")
+print(f"Random Forest Regression - mae: {mae}")
